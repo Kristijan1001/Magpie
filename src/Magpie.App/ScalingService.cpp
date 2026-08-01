010b101af9a3130adfd0a0fbeefb22d97878f71e
Kristijan1001 <24441639+Kristijan1001@users.noreply.github.com>
2026-08-01 22:15:07 +0200

    feat: user-specified max size for dynamic TensorRT engines

--- src/Magpie.App/ScalingService.cpp
+++ src/Magpie.App/ScalingService.cpp
@@ -254,6 +254,8 @@ bool ScalingService::_StartScale(HWND hWnd, const Profile& profile) {
 	options.onnxScale = profile.onnxScale;
 	options.onnxBackend = profile.onnxBackend;
 	options.onnxStaticEngine = profile.onnxStaticEngine;
+	options.onnxDynamicMaxWidth = profile.onnxDynamicMaxWidth;
+	options.onnxDynamicMaxHeight = profile.onnxDynamicMaxHeight;
 	if (profile.isFrameRateLimiterEnabled) {
 		options.maxFrameRate = profile.maxFrameRate;
 	}
